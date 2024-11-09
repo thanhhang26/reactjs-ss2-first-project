86071a74e13e7b0e9e704dd725f06a5f59e64b03
ss3_code <dinhhang440@gmail.com>
2024-11-09 13:06:50 +0700

    change

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const list1 = React.createElement("li", null, "Thành phố Hà Nội");
+const list2 = React.createElement("li", null, "Thành phố Đà Nẵng");
+const list3 = React.createElement("li", null, "Thành phố Hải Phòng");
+const list4 = React.createElement("li", null, "Thành phố Hồ Chí Minh");
+const list5 = React.createElement("li", null, "Thành phố Cần Thơ");
+
+const listMain = React.createElement("ul", null, list1, list2, list3, list4, list5);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(listMain);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
